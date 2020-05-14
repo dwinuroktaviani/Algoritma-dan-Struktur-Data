c027a86a4637925878f404a6efa58dfa9ad427d1
addinanm <addinanurulmawaddah39@gmail.com>
2020-05-14 11:41:59 +0700

    upload jobsheet 14

---
+++ Pertemuan14/Pertemuan 14/StrukturData/src/BinaryTree/Node.java
@@ -0,0 +1,19 @@
+
+package BinaryTree;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node() {
+    }
+
+    public Node(int data) {
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+    
+    
+}
