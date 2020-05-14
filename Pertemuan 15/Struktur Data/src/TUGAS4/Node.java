33d16873d59cfbeb1ae9403c615374e715f7008c
okta18 <60720756+okta18@users.noreply.github.com>
2020-05-14 16:25:45 +0700

    Add files via upload

---
+++ Pertemuan 15/Struktur Data/src/TUGAS4/Node.java
@@ -0,0 +1,15 @@
+
+package TUGAS4;
+
+public class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    Node(Node prev, int data, Node next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+
+}
